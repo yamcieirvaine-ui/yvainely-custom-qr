ba490fc445aa25a36e9aa9c2d2472f4038b28f56
yamcieirvaine-ui <yamcieirvaine@gmail.com>
2026-09-06 02:24:02 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Yvainely Custom QR",
+  description: "Custom QR generator",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
